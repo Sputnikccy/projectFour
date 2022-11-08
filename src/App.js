05702069eb42f51a75e170480c1de547e1a04d0b
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-11-08 12:19:06 -0500

    setting up and testing sass, partial creation of form.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import './App.scss';
 import Header from './components/Header';
 import { Link, Routes, Route } from 'react-router-dom'
 import TestComponent from './components/TestComponent';
-import BoredForm from './components/BoredForm';
+import BoredForm from './components/Forms/BoredForm';
 
 function App() {
 
--- src/components/BoredForm.js
+++
@@ -1,7 +0,0 @@
-const BoredForm = () => {
-    return(
-        <h4>Hello this is the form.</h4>
-    )
-}
-
-export default BoredForm
