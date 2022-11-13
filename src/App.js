0d761f29b5927536ce6262f5ebeef05701e6bf1a
tri-ma <103957249+tri-ma@users.noreply.github.com>
2022-11-12 20:46:55 -0500

    Created ticketmaster.js, made API call, and started filter feature logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './sass/App.scss';
 import { Link, Routes, Route } from 'react-router-dom'
 // components
 import Header from './components/Header';
-import TestComponent from './components/TestComponent';
+// import TestComponent from './components/TestComponent';
+import Ticketmaster from  './components/Ticketmaster';
 // import BoredForm from './components/Forms/BoredForm';
 import LandingPage from './components/LandingPage'
 import BoredComponent from './components/BoredComponent';
@@ -21,7 +22,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage />}/>
         {/* TicketMaster component will replace the below component, this was just for testing routing. */}
-        <Route path='/testComponent' element={<TestComponent/>}/>
+        <Route path='/ticketmaster' element={<Ticketmaster/>}/>
         <Route path='/boredComponent' element={<BoredComponent/>}/>
       </Routes>
       
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () =>{
                 <label>Are you willing to spend money?</label>
 
                 <div className='button-container'>
-                    <Link to="/testComponent">
+                    <Link to="/ticketmaster">
                         <button>Yes</button>
                     </Link>
                     
