6dc654a20339ad45d217833b861aa552d00115e6
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-11-14 12:42:20 -0500

    latest commit for people to pull

--- src/components/BoredComponent.js
+++ src/components/BoredComponent.js
@@ -1,80 +1,110 @@
 import { Link } from 'react-router-dom';
 import axios from "axios"
 import { useState, useEffect } from 'react'
+// import renderImage from '../Assets/lion.jpg'
 
 const BoredComponent = () => {
 
-    // useEffect((i) => {
-    //     let tempArray = []
 
-    //     for (i = 0; i <= 7; i++){
-    //         axios({
-    //             url: `http://www.boredapi.com/api/activity`,
-    //             method: 'GET',
-    //             dataResponse: 'json'
-    
-    //         })
-        
-    //         .then((response) => {
-    //             tempArray.push(response.data)
-    //             console.log(tempArray)
-    //             setActivities(tempArray);
-    //         })
-    //     }
-    // }, [])
-    
-    useEffect((i) => {
-        let tempArray = []
-
-        for (i = 0; i <= 10; i++){
-            axios({
-                url: `http://www.boredapi.com/api/activity`,
-                method: 'GET',
-                dataResponse: 'json'
-            })
+    const getActivity = (e) => {
+        e.preventDefault()
+        const options = {
+            url: `http://www.boredapi.com/api/activity?type=${userChoice}`,
+            method: 'GET',
+            dataResponse: 'json'
+        };
+        axios.request(options)
             .then((response) => {
-                //checks array to see if there is already an activity
-                if (!tempArray.includes(response.data)){
-                    //pushes if there isnt
-                    tempArray.push(response.data)
-                }  
-                //.then once the 10 api calls are finished to set activities 
-            }).then (()=>{
-                
-                setActivities(tempArray)
-        })}
-    }, [])
-
-
-    const [activities, setActivities] = useState("");
-    console.log(activities)
-
-
-
-    if (activities.length === 0) {
-        return <p>Loading...</p>
+                console.log(response.data)
+                setActivities(response.data);
+                getPicture(response.data.activity)
+            })
+            // getPicture()
+            console.log("this is the activities; ",activities.activity)
+            
     }
 
-    return (
-        <div>
-            <div>
-            {
-                activities.map((choice) => {
-                    return (
-                        <li id={choice.id} key={choice.key}>
-                            <h3>{choice.activity}</h3>
-                            <Link to={`/boredForm/${choice.key}`}>
-                                <button>Create event</button>
-                            </Link>
-                        </li>
-                        
-                    )
-                })
+    const getPicture = (info) => {
+        axios({
+            url: 'https://api.unsplash.com/search/photos',
+            method: 'GET',
+            dataResponse: 'json',
+            params: {
+                client_id: 'xMApnHMvGsHXF8WNkU53mf3KirR2oQ8ZS6YYr-M-NAU',
+                query: info,
+                per_page: 1
             }
+        }).then((response) =>{
+            console.log(response)
+            // setActivityImage(response.data.results[0])
+            const apiImage = response.data.results[0].urls.thumb;
+            setActivityImage(apiImage)
+        })
+    }
+    
+
+
+
+
+    //track data from API
+    const [activities, setActivities] = useState("");
+    //track data of user's choice of event type
+    const [userChoice, setUserChoice] = useState("")
+    
+    const [activityImage, setActivityImage] = useState({})
+
+    // const placeHolderImage = renderImage
+
+    
+    const handleUserChoice = (e) => {
+        setUserChoice(e.target.value);
+    }
+    
+
+   
+    
+    return (
+        <>
+            <h3>Testing</h3>
+            <form action="submit" onSubmit={getActivity}>
+                {/* sr-only not working here, need to be figured out later */}
+                <select
+                    id='choice'
+                    onChange={handleUserChoice}
+                    value={userChoice}
+                >
+                    <option value="title" selected>Select Type</option>
+                    <option value="education">education</option>
+                    <option value="recreational">recreational</option>
+                    <option value="social">social</option>
+                    <option value="diy">diy</option>
+                    <option value="charity">charity</option>
+                </select>
+                {/* sr-only not working here, need to be figured out later */}
+                
+
+                <button >Click for activities</button>
+            </form>
+
+            <div>
+                {activityImage?
+                // <img src={placeHolderImage}/>
+                <img src={activityImage}  />
+                :
+                "no picture"}
+                
+                
+                    
+                    <h3>{activities.activity}</h3>
+                    <h4>{activities.type}</h4>
+                    <h5>{activities.participants}</h5>
+                    <Link to={`/boredForm/${activities.key}`}>
+                        <button>Create event</button>
+                    </Link>
             </div>
-        </div>
+        </>
     );
+
 }
 
 export default BoredComponent
-
