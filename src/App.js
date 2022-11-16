41d26e5986fb110a234513f4a6f3649b6b56e22e
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-11-16 16:56:08 -0500

    finally merged all work.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@
 import './sass/App.scss';
 // utilities
 import { Link, Routes, Route } from 'react-router-dom'
-// import { useState, useEffect } from 'react';
-// import { getDatabase, ref, push, onValue } from 'firebase/database';
 // components
-
-import Header from './components/Header';
 import BoredForm from './components/Forms/BoredForm';
 import LandingPage from './components/LandingPage'
 import BoredComponent from './components/BoredComponent';
@@ -22,11 +18,8 @@ import DisplayAllTmEvents from './components/DisplayAllTmEvents'
 function App() {
   return (
     <div className="App">
-     
       <Routes>
         <Route path='/' element={<LandingPage />}/>
-        {/* TicketMaster component will replace the below component, this was just for testing routing. */}
-        {/* <Route path='/testComponent' element={<TestComponent/>}/> */}
         <Route path='/ticketMaster' element={<TicketMaster/>} />
         <Route path='/displayAllEvents' element={<DisplayAllEvents />}>
           <Route path='/displayAllEvents/displayAllTmEvents' element={<DisplayAllTmEvents/>}/>
