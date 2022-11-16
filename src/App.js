e4d7fd10fb101d4f1808060555338c07507881e6
Caiyi <plutoccy@gmail.com>
2022-11-16 14:41:52 -0700

    display recents events to let users have refernces

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import { Link, Routes, Route } from 'react-router-dom'
 // import { useState, useEffect } from 'react';
 // import { getDatabase, ref, push, onValue } from 'firebase/database';
 // components
-import Header from './components/Header';
-// import TestComponent from './components/TestComponent';
+
 import BoredForm from './components/Forms/BoredForm';
 import LandingPage from './components/LandingPage'
 import BoredComponent from './components/BoredComponent';
@@ -14,22 +13,22 @@ import TicketMaster from './components/TicketMaster'
 import TmTestForm from './components/TmTestForm';
 import TmTestCard from './components/TmTestCard';
 import BoredInvite from './components/Forms/BoredInvite';
-// import app from './firebase';
-// import { useNavigate } from "react-router-dom";
+import DisplayAllEvents from './components/DisplayAllEvents'
+import DisplayAllBoredEvents from './components/DisplayAllBoredEvents'
+import DisplayAllTmEvents from './components/DisplayAllTmEvents'
 
 
 function App() {
-
-
   return (
     <div className="App">
-      <Link to="/">
-        <Header />
-      </Link>
+     
       <Routes>
         <Route path='/' element={<LandingPage />}/>
-        {/* TicketMaster component will replace the below component, this was just for testing routing. */}
-        {/* <Route path='/testComponent' element={<TestComponent/>}/> */}
+        <Route path='/displayAllEvents' element={<DisplayAllEvents />}>
+          <Route path='/displayAllEvents/displayAllTmEvents' element={<DisplayAllTmEvents/>}/>
+          <Route path='/displayAllEvents/displayAllBoredEvents' element={<DisplayAllBoredEvents />}/>
+        </Route>
+        
         <Route path='/ticketMaster' element={<TicketMaster/>} />
         <Route path='/tmform/:idd' element={<TmTestForm/>}/>
         <Route path='/tmcard/:idd' element={<TmTestCard/>}/>
